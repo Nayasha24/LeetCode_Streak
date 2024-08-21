f632aaaf6103cfbf1758b0b8975add1bb8039984
Nayasha Rathi <114497250+Nayasha24@users.noreply.github.com>
2024-08-21 19:08:12 +0530

    Time: 6 ms (75.00%) | Memory: 21.3 MB (45.66%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    static int removeDuplicates(vector<int>& nums) {
         if(nums.empty())return 0;
         int i=1;
         int n = nums.size();
@@ -13,3 +13,8 @@ public:
         return i;
     }
 };
+auto init=[](){
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return ios_base::sync_with_stdio(0);
+}();
