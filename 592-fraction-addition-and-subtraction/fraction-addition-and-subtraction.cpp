96eb90e4176ef515f77e098f8f79da7eaf2def67
Nayasha Rathi <114497250+Nayasha24@users.noreply.github.com>
2024-08-23 11:03:32 +0530

    Time: 3 ms (50.39%) | Memory: 7.8 MB (58.91%) - LeetSync

---
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    using int2=pair<int, int>;
+    void print(auto& f){
+        for(auto& [x, y]: f) cout<<"("<<x<<","<<y<<")";
+        cout<<endl;
+    }
+
+    vector<int2> convert(string& expression){
+        vector<int2> fraction;
+        stringstream ss(expression);
+        char op;
+        int num, denom;
+
+        while(ss>>num>>op>>denom){
+            fraction.emplace_back(num, denom);
+        }
+
+        return fraction;
+    }
+    int2 add(int2& x, int2& y){
+        auto [xp, xq]=x;
+        auto [yp, yq]=y;
+        long long q=xq*yq;
+        long long p=xp*yq+xq*yp;
+        long long g=gcd(p, q);
+        return {p/g, q/g};
+    }
+    
+    string fractionAddition(string& expression) {
+        auto fraction=convert(expression);
+    //    print(fraction);
+        int fz=fraction.size();
+        int2 ans={0, 1};
+        for(auto& f: fraction){
+            ans=add(ans, f);
+        }
+        string s=to_string(ans.first)+"/"+to_string(ans.second);
+        return s;
+    }
+};
